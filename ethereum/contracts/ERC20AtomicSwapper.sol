20408358b354969f740bc23b25f62b15a7b33177
HaoyangLiu <lhy.0318@163.com>
2019-08-23 12:19:28 +0800

    Resolve issue from audit, change secretHash to randomNumberHash, change secretKey to randomNumber

--- ethereum/contracts/ERC20AtomicSwapper.sol
+++ ethereum/contracts/ERC20AtomicSwapper.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.8;
+pragma solidity 0.5.8;
 
 interface ERC20 {
     function totalSupply() external view returns (uint);
@@ -27,9 +27,9 @@ contract ERC20AtomicSwapper {
     }
 
     // Events
-    event HTLT(address indexed _msgSender, address indexed _receiverAddr, bytes32 indexed _secretHashLock, uint64 _timestamp, bytes20 _bep2Addr, uint256 _expireHeight, uint256 _outAmount, uint256 _bep2Amount);
-    event Refunded(address indexed _msgSender, address indexed _swapSender, bytes32 indexed _secretHashLock);
-    event Claimed(address indexed _msgSender, address indexed _receiverAddr, bytes32 indexed _secretHashLock, bytes32 _secretKey);
+    event HTLT(address indexed _msgSender, address indexed _receiverAddr, bytes32 indexed _randomNumberHash, uint64 _timestamp, bytes20 _bep2Addr, uint256 _expireHeight, uint256 _outAmount, uint256 _bep2Amount);
+    event Refunded(address indexed _msgSender, address indexed _swapSender, bytes32 indexed _randomNumberHash);
+    event Claimed(address indexed _msgSender, address indexed _receiverAddr, bytes32 indexed _randomNumberHash, bytes32 _randomNumber);
 
     // Storage
     mapping (bytes32 => Swap) private swaps;
@@ -38,34 +38,32 @@ contract ERC20AtomicSwapper {
     address public ERC20ContractAddr;
 
     /// @notice Throws if the swap is not invalid (i.e. has already been used)
-    modifier onlyInvalidSwaps(bytes32 _secretHashLock) {
-        require(swapStates[_secretHashLock] == States.INVALID, "swap is opened previously");
+    modifier onlyInvalidSwaps(bytes32 _randomNumberHash) {
+        require(swapStates[_randomNumberHash] == States.INVALID, "swap is opened previously");
         _;
     }
 
     /// @notice Throws if the swap is not open.
-    modifier onlyOpenSwaps(bytes32 _secretHashLock) {
-        require(swapStates[_secretHashLock] == States.OPEN, "swap is not opened");
+    modifier onlyOpenSwaps(bytes32 _randomNumberHash) {
+        require(swapStates[_randomNumberHash] == States.OPEN, "swap is not opened");
         _;
     }
 
     /// @notice Throws if the swap is already expired.
-    modifier onlyAfterExpireHeight(bytes32 _secretHashLock) {
-        /* solium-disable-next-line security/no-block-members */
-        require(block.number >= swaps[_secretHashLock].expireHeight, "swap is not expired");
+    modifier onlyAfterExpireHeight(bytes32 _randomNumberHash) {
+        require(block.number >= swaps[_randomNumberHash].expireHeight, "swap is not expired");
         _;
     }
 
     /// @notice Throws if the expireHeight is reached
-    modifier onlyBeforeExpireHeight(bytes32 _secretHashLock) {
-        /* solium-disable-next-line security/no-block-members */
-        require(block.number < swaps[_secretHashLock].expireHeight, "swap is already expired");
+    modifier onlyBeforeExpireHeight(bytes32 _randomNumberHash) {
+        require(block.number < swaps[_randomNumberHash].expireHeight, "swap is already expired");
         _;
     }
 
-    /// @notice Throws if the secret key is not valid.
-    modifier onlyWithSecretKey(bytes32 _secretHashLock, bytes32 _secretKey) {
-        require(_secretHashLock == calSecretHash(_secretKey, swaps[_secretHashLock].timestamp), "invalid secretKey");
+    /// @notice Throws if the random number is not valid.
+    modifier onlyWithRandomNumber(bytes32 _randomNumberHash, bytes32 _randomNumber) {
+        require(_randomNumberHash == calRandomNumberHash(_randomNumber, swaps[_randomNumberHash].timestamp), "invalid randomNumber");
         _;
     }
 
@@ -76,7 +74,7 @@ contract ERC20AtomicSwapper {
 
     /// @notice hashTimerLockedTransfer locks asset to contract address and create an atomic swap.
     ///
-    /// @param _secretHashLock The hash of the secret key and timestamp
+    /// @param _randomNumberHash The hash of the random number and timestamp
     /// @param _timestamp Counted by second
     /// @param _heightSpan The number of blocks to wait before the asset can be returned to sender
     /// @param _receiverAddr The ethereum address of the swap counterpart.
@@ -84,21 +82,20 @@ contract ERC20AtomicSwapper {
     /// @param _outAmount ERC20 asset to swap out.
     /// @param _bep2Amount BEP2 asset to swap in.
     function htlt(
-        bytes32 _secretHashLock,
+        bytes32 _randomNumberHash,
         uint64  _timestamp,
         uint256 _heightSpan,
         address _receiverAddr,
         bytes20 _bep2Addr,
         uint256 _outAmount,
         uint256 _bep2Amount
-    ) external onlyInvalidSwaps(_secretHashLock) returns (bool) {
+    ) external onlyInvalidSwaps(_randomNumberHash) returns (bool) {
+        require(_outAmount > 0, "_outAmount must be more than 0");
         // Assume average block time interval is 10 second
         // The heightSpan period should be more than 10 minutes and less than one week
         require(_heightSpan >= 60 && _heightSpan <= 60480, "_heightSpan should be in [60, 60480]");
         require(_receiverAddr != address(0), "_receiverAddr should not be zero");
         require(_timestamp > now -7200 && _timestamp < now + 3600, "The timestamp should not be one hour ahead or two hour behind current time");
-        // Transfer ERC20 token to the swap contract
-        require(ERC20(ERC20ContractAddr).transferFrom(msg.sender, address(this), _outAmount), "failed to transfer client asset to swap contract address");
         // Store the details of the swap.
         Swap memory swap = Swap({
             outAmount: _outAmount,
@@ -108,59 +105,65 @@ contract ERC20AtomicSwapper {
             receiverAddr: _receiverAddr
         });
 
-        swaps[_secretHashLock] = swap;
-        swapStates[_secretHashLock] = States.OPEN;
+        swaps[_randomNumberHash] = swap;
+        swapStates[_randomNumberHash] = States.OPEN;
+
+        // Transfer ERC20 token to the swap contract
+        require(ERC20(ERC20ContractAddr).transferFrom(msg.sender, address(this), _outAmount), "failed to transfer client asset to swap contract address");
 
         // Emit initialization event
-        emit HTLT(msg.sender, _receiverAddr, _secretHashLock, _timestamp, _bep2Addr, swap.expireHeight, _outAmount, _bep2Amount);
+        emit HTLT(msg.sender, _receiverAddr, _randomNumberHash, _timestamp, _bep2Addr, swap.expireHeight, _outAmount, _bep2Amount);
         return true;
     }
 
     /// @notice claim claims the previously locked asset.
     ///
-    /// @param _secretHashLock The hash of secretKey and timestamp
-    /// @param _secretKey The secret of the atomic swap.
-    function claim(bytes32 _secretHashLock, bytes32 _secretKey) external onlyOpenSwaps(_secretHashLock) onlyBeforeExpireHeight(_secretHashLock) onlyWithSecretKey(_secretHashLock, _secretKey) returns (bool) {
+    /// @param _randomNumberHash The hash of randomNumber and timestamp
+    /// @param _randomNumber The random number
+    function claim(bytes32 _randomNumberHash, bytes32 _randomNumber) external onlyOpenSwaps(_randomNumberHash) onlyBeforeExpireHeight(_randomNumberHash) onlyWithRandomNumber(_randomNumberHash, _randomNumber) returns (bool) {
         // Complete the swap.
-        swapStates[_secretHashLock] = States.COMPLETED;
+        swapStates[_randomNumberHash] = States.COMPLETED;
+
+        address receiverAddr = swaps[_randomNumberHash].receiverAddr;
+        uint256 outAmount = swaps[_randomNumberHash].outAmount;
+        // delete closed swap
+        delete swaps[_randomNumberHash];
 
         // Pay erc20 token to receiver
-        require(ERC20(ERC20ContractAddr).transfer(swaps[_secretHashLock].receiverAddr, swaps[_secretHashLock].outAmount), "Failed to transfer locked asset to receiver");
-
-        address receiverAddr = swaps[_secretHashLock].receiverAddr;
-        // delete closed swap
-        delete swaps[_secretHashLock];
+        require(ERC20(ERC20ContractAddr).transfer(receiverAddr, outAmount), "Failed to transfer locked asset to receiver");
 
         // Emit completion event
-        emit Claimed(msg.sender, receiverAddr, _secretHashLock, _secretKey);
+        emit Claimed(msg.sender, receiverAddr, _randomNumberHash, _randomNumber);
 
         return true;
     }
 
     /// @notice refund refunds the previously locked asset.
     ///
-    /// @param _secretHashLock The hash of secretKey and timestamp
-    function refund(bytes32 _secretHashLock) external onlyOpenSwaps(_secretHashLock) onlyAfterExpireHeight(_secretHashLock) returns (bool) {
+    /// @param _randomNumberHash The hash of randomNumber and timestamp
+    function refund(bytes32 _randomNumberHash) external onlyOpenSwaps(_randomNumberHash) onlyAfterExpireHeight(_randomNumberHash) returns (bool) {
         // Expire the swap.
-        swapStates[_secretHashLock] = States.EXPIRED;
+        swapStates[_randomNumberHash] = States.EXPIRED;
+
+        address swapSender = swaps[_randomNumberHash].sender;
+        uint256 outAmount = swaps[_randomNumberHash].outAmount;
+        // delete closed swap
+        delete swaps[_randomNumberHash];
 
         // refund erc20 token to swap creator
-        require(ERC20(ERC20ContractAddr).transfer(swaps[_secretHashLock].sender, swaps[_secretHashLock].outAmount), "Failed to transfer locked asset back to swap creator");
+        require(ERC20(ERC20ContractAddr).transfer(swapSender, outAmount), "Failed to transfer locked asset back to swap creator");
 
-        address swapSender = swaps[_secretHashLock].sender;
-        // delete closed swap
-        delete swaps[_secretHashLock];
         // Emit expire event
-        emit Refunded(msg.sender, swapSender, _secretHashLock);
+        emit Refunded(msg.sender, swapSender, _randomNumberHash);
 
         return true;
     }
 
-    /// @notice query an atomic swap by secretHashLock
+    /// @notice query an atomic swap by randomNumberHash
     ///
-    /// @param _secretHashLock The hash of secretKey and timestamp
-    function queryOpenSwap(bytes32 _secretHashLock) external view returns(uint64 _timestamp, uint256 _expireHeight, uint256 _outAmount, address _sender, address _receiver) {
-        Swap memory swap = swaps[_secretHashLock];
+    /// @param _randomNumberHash The hash of randomNumber and timestamp
+    function queryOpenSwap(bytes32 _randomNumberHash) external view returns(uint64 _timestamp, uint256 _expireHeight, uint256 _outAmount, address _sender, address _receiver) {
+        Swap memory swap = swaps[_randomNumberHash];
         return (
             swap.timestamp,
             swap.expireHeight,
@@ -170,33 +173,32 @@ contract ERC20AtomicSwapper {
         );
     }
 
-    /// @notice Checks whether a _secretHashLock can be used to create a hash lock or not.
+    /// @notice Checks whether a _randomNumberHash can be used to create a hash lock or not.
     ///
-    /// @param _secretHashLock The hash of secretKey and timestamp
-    function hashLockable(bytes32 _secretHashLock) external view returns (bool) {
-        return (swapStates[_secretHashLock] == States.INVALID);
+    /// @param _randomNumberHash The hash of randomNumber and timestamp
+    function hashLockable(bytes32 _randomNumberHash) external view returns (bool) {
+        return (swapStates[_randomNumberHash] == States.INVALID);
     }
 
     /// @notice Checks whether a swap is refundable or not.
     ///
-    /// @param _secretHashLock The hash of secretKey and timestamp
-    function refundable(bytes32 _secretHashLock) external view returns (bool) {
-        /* solium-disable-next-line security/no-block-members */
-        return (block.number >= swaps[_secretHashLock].expireHeight && swapStates[_secretHashLock] == States.OPEN);
+    /// @param _randomNumberHash The hash of randomNumber and timestamp
+    function refundable(bytes32 _randomNumberHash) external view returns (bool) {
+        return (block.number >= swaps[_randomNumberHash].expireHeight && swapStates[_randomNumberHash] == States.OPEN);
     }
 
     /// @notice Checks whether a swap is claimable or not.
     ///
-    /// @param _secretHashLock The hash of secretKey and timestamp
-    function claimable(bytes32 _secretHashLock) external view returns (bool) {
-        return (block.number < swaps[_secretHashLock].expireHeight && swapStates[_secretHashLock] == States.OPEN);
+    /// @param _randomNumberHash The hash of randomNumber and timestamp
+    function claimable(bytes32 _randomNumberHash) external view returns (bool) {
+        return (block.number < swaps[_randomNumberHash].expireHeight && swapStates[_randomNumberHash] == States.OPEN);
     }
 
-    /// @notice Calculate the secretHashLock from secretKey and timestamp
+    /// @notice Calculate the randomNumberHash from randomNumber and timestamp
     ///
-    /// @param _secretKey The secret.
+    /// @param _randomNumber The random number.
     /// @param _timestamp The timestamp.
-    function calSecretHash(bytes32 _secretKey, uint64 _timestamp) public pure returns (bytes32) {
-        return sha256(abi.encodePacked(_secretKey, _timestamp));
+    function calRandomNumberHash(bytes32 _randomNumber, uint64 _timestamp) public pure returns (bytes32) {
+        return sha256(abi.encodePacked(_randomNumber, _timestamp));
     }
 }
--- ethereum/contracts/ETHAtomicSwapper.sol
+++ ethereum/contracts/ETHAtomicSwapper.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.8;
+pragma solidity 0.5.8;
 
 contract ETHAtomicSwapper {
 
@@ -18,62 +18,61 @@ contract ETHAtomicSwapper {
     }
 
     // Events
-    event HTLT(address indexed _msgSender, address indexed _receiverAddr, bytes32 indexed _secretHashLock, uint64 _timestamp, bytes20 _bep2Addr, uint256 _expireHeight, uint256 _outAmount, uint256 _bep2Amount);
-    event Refunded(address indexed _msgSender, address indexed _swapSender, bytes32 indexed _secretHashLock);
-    event Claimed(address indexed _msgSender, address indexed _receiverAddr, bytes32 indexed _secretHashLock, bytes32 _secretKey);
+    event HTLT(address indexed _msgSender, address indexed _receiverAddr, bytes32 indexed _randomNumberHash, uint64 _timestamp, bytes20 _bep2Addr, uint256 _expireHeight, uint256 _outAmount, uint256 _bep2Amount);
+    event Refunded(address indexed _msgSender, address indexed _swapSender, bytes32 indexed _randomNumberHash);
+    event Claimed(address indexed _msgSender, address indexed _receiverAddr, bytes32 indexed _randomNumberHash, bytes32 _randomNumber);
 
     // Storage
     mapping (bytes32 => Swap) private swaps;
     mapping (bytes32 => States) private swapStates;
 
     /// @notice Throws if the swap is not invalid (i.e. has already been used)
-    modifier onlyInvalidSwaps(bytes32 _secretHashLock) {
-        require(swapStates[_secretHashLock] == States.INVALID, "swap is opened previously");
+    modifier onlyInvalidSwaps(bytes32 _randomNumberHash) {
+        require(swapStates[_randomNumberHash] == States.INVALID, "swap is opened previously");
         _;
     }
 
     /// @notice Throws if the swap is not open.
-    modifier onlyOpenSwaps(bytes32 _secretHashLock) {
-        require(swapStates[_secretHashLock] == States.OPEN, "swap is not opened");
+    modifier onlyOpenSwaps(bytes32 _randomNumberHash) {
+        require(swapStates[_randomNumberHash] == States.OPEN, "swap is not opened");
         _;
     }
 
     /// @notice Throws if the swap is already expired.
-    modifier onlyAfterExpireHeight(bytes32 _secretHashLock) {
-        /* solium-disable-next-line security/no-block-members */
-        require(block.number >= swaps[_secretHashLock].expireHeight, "swap is not expired");
+    modifier onlyAfterExpireHeight(bytes32 _randomNumberHash) {
+        require(block.number >= swaps[_randomNumberHash].expireHeight, "swap is not expired");
         _;
     }
 
     /// @notice Throws if the expireHeight is reached
-    modifier onlyBeforeExpireHeight(bytes32 _secretHashLock) {
-        /* solium-disable-next-line security/no-block-members */
-        require(block.number < swaps[_secretHashLock].expireHeight, "swap is already expired");
+    modifier onlyBeforeExpireHeight(bytes32 _randomNumberHash) {
+        require(block.number < swaps[_randomNumberHash].expireHeight, "swap is already expired");
         _;
     }
 
-    /// @notice Throws if the secret key is not valid.
-    modifier onlyWithSecretKey(bytes32 _secretHashLock, bytes32 _secretKey) {
-        require(_secretHashLock == calSecretHash(_secretKey, swaps[_secretHashLock].timestamp), "invalid secretKey");
+    /// @notice Throws if the random number is not valid.
+    modifier onlyWithRandomNumber(bytes32 _randomNumberHash, bytes32 _randomNumber) {
+        require(_randomNumberHash == calRandomNumberHash(_randomNumber, swaps[_randomNumberHash].timestamp), "invalid randomNumber");
         _;
     }
 
     /// @notice HTLT locks asset to contract address and create an atomic swap.
     ///
-    /// @param _secretHashLock The hash of the secret key and timestamp
+    /// @param _randomNumberHash The hash of the random number and timestamp
     /// @param _timestamp Counted by second
     /// @param _heightSpan The number of blocks to wait before the asset can be returned to sender
     /// @param _receiverAddr The ethereum address of the swap counterpart.
     /// @param _bep2Addr The receiver address on Binance Chain
     /// @param _bep2Amount BEP2 asset to swap in.
     function htlt(
-        bytes32 _secretHashLock,
+        bytes32 _randomNumberHash,
         uint64  _timestamp,
         uint256 _heightSpan,
         address payable _receiverAddr,
         bytes20 _bep2Addr,
         uint256 _bep2Amount
-    ) external onlyInvalidSwaps(_secretHashLock) payable returns (bool) {
+    ) external onlyInvalidSwaps(_randomNumberHash) payable returns (bool) {
+        require(msg.value > 0, "msg.value must be more than 0");
         // Assume average block time interval is 10 second
         // The heightSpan period should be more than 10 minutes and less than one week
         require(_heightSpan >= 60 && _heightSpan <= 60480, "_heightSpan should be in [60, 60480]");
@@ -88,62 +87,62 @@ contract ETHAtomicSwapper {
             receiverAddr: _receiverAddr
         });
 
-        swaps[_secretHashLock] = swap;
-        swapStates[_secretHashLock] = States.OPEN;
+        swaps[_randomNumberHash] = swap;
+        swapStates[_randomNumberHash] = States.OPEN;
 
         // Emit initialization event
-        emit HTLT(msg.sender, _receiverAddr, _secretHashLock, _timestamp, _bep2Addr, swap.expireHeight, msg.value, _bep2Amount);
+        emit HTLT(msg.sender, _receiverAddr, _randomNumberHash, _timestamp, _bep2Addr, swap.expireHeight, msg.value, _bep2Amount);
         return true;
     }
 
     /// @notice claim claims the previously locked asset.
     ///
-    /// @param _secretHashLock The hash of secretKey and timestamp
-    /// @param _secretKey The secret of the atomic swap.
-    function claim(bytes32 _secretHashLock, bytes32 _secretKey) external onlyOpenSwaps(_secretHashLock) onlyBeforeExpireHeight(_secretHashLock) onlyWithSecretKey(_secretHashLock, _secretKey) returns (bool) {
+    /// @param _randomNumberHash The hash of randomNumber and timestamp
+    /// @param _randomNumber The random number
+    function claim(bytes32 _randomNumberHash, bytes32 _randomNumber) external onlyOpenSwaps(_randomNumberHash) onlyBeforeExpireHeight(_randomNumberHash) onlyWithRandomNumber(_randomNumberHash, _randomNumber) returns (bool) {
         // Complete the swap.
-        swapStates[_secretHashLock] = States.COMPLETED;
+        swapStates[_randomNumberHash] = States.COMPLETED;
 
         // Pay eth coin to receiver
-        swaps[_secretHashLock].receiverAddr.transfer(swaps[_secretHashLock].outAmount);
+        swaps[_randomNumberHash].receiverAddr.transfer(swaps[_randomNumberHash].outAmount);
 
-        address receiverAddr = swaps[_secretHashLock].receiverAddr;
+        address receiverAddr = swaps[_randomNumberHash].receiverAddr;
 
         // delete closed swap
-        delete swaps[_secretHashLock];
+        delete swaps[_randomNumberHash];
 
         // Emit completion event
-        emit Claimed(msg.sender, receiverAddr, _secretHashLock, _secretKey);
+        emit Claimed(msg.sender, receiverAddr, _randomNumberHash, _randomNumber);
 
         return true;
     }
 
     /// @notice refund refunds the previously locked asset.
     ///
-    /// @param _secretHashLock The hash of secretKey and timestamp
-    function refund(bytes32 _secretHashLock) external onlyOpenSwaps(_secretHashLock) onlyAfterExpireHeight(_secretHashLock) returns (bool) {
+    /// @param _randomNumberHash The hash of randomNumber and timestamp
+    function refund(bytes32 _randomNumberHash) external onlyOpenSwaps(_randomNumberHash) onlyAfterExpireHeight(_randomNumberHash) returns (bool) {
         // Expire the swap.
-        swapStates[_secretHashLock] = States.EXPIRED;
+        swapStates[_randomNumberHash] = States.EXPIRED;
 
         // refund eth coin to swap creator
-        swaps[_secretHashLock].sender.transfer(swaps[_secretHashLock].outAmount);
+        swaps[_randomNumberHash].sender.transfer(swaps[_randomNumberHash].outAmount);
 
-        address swapSender = swaps[_secretHashLock].sender;
+        address swapSender = swaps[_randomNumberHash].sender;
 
         // delete closed swap
-        delete swaps[_secretHashLock];
+        delete swaps[_randomNumberHash];
 
         // Emit expire event
-        emit Refunded(msg.sender, swapSender, _secretHashLock);
+        emit Refunded(msg.sender, swapSender, _randomNumberHash);
 
         return true;
     }
 
-    /// @notice query an atomic swap by secretHashLock
+    /// @notice query an atomic swap by randomNumberHash
     ///
-    /// @param _secretHashLock The hash of secretKey and timestamp
-    function queryOpenSwap(bytes32 _secretHashLock) external view returns(uint64 _timestamp, uint256 _expireHeight, uint256 _outAmount, address _sender, address _receiver) {
-        Swap memory swap = swaps[_secretHashLock];
+    /// @param _randomNumberHash The hash of randomNumber and timestamp
+    function queryOpenSwap(bytes32 _randomNumberHash) external view returns(uint64 _timestamp, uint256 _expireHeight, uint256 _outAmount, address _sender, address _receiver) {
+        Swap memory swap = swaps[_randomNumberHash];
         return (
             swap.timestamp,
             swap.expireHeight,
@@ -153,33 +152,32 @@ contract ETHAtomicSwapper {
         );
     }
 
-    /// @notice Checks whether a _secretHashLock can be used to create a hash lock or not.
+    /// @notice Checks whether a _randomNumberHash can be used to create a hash lock or not.
     ///
-    /// @param _secretHashLock The hash of secretKey and timestamp
-    function hashLockable(bytes32 _secretHashLock) external view returns (bool) {
-        return (swapStates[_secretHashLock] == States.INVALID);
+    /// @param _randomNumberHash The hash of randomNumber and timestamp
+    function hashLockable(bytes32 _randomNumberHash) external view returns (bool) {
+        return (swapStates[_randomNumberHash] == States.INVALID);
     }
 
     /// @notice Checks whether a swap is refundable or not.
     ///
-    /// @param _secretHashLock The hash of secretKey and timestamp
-    function refundable(bytes32 _secretHashLock) external view returns (bool) {
-        /* solium-disable-next-line security/no-block-members */
-        return (block.number >= swaps[_secretHashLock].expireHeight && swapStates[_secretHashLock] == States.OPEN);
+    /// @param _randomNumberHash The hash of randomNumber and timestamp
+    function refundable(bytes32 _randomNumberHash) external view returns (bool) {
+        return (block.number >= swaps[_randomNumberHash].expireHeight && swapStates[_randomNumberHash] == States.OPEN);
     }
 
     /// @notice Checks whether a swap is claimable or not.
     ///
-    /// @param _secretHashLock The hash of secretKey and timestamp
-    function claimable(bytes32 _secretHashLock) external view returns (bool) {
-        return (block.number < swaps[_secretHashLock].expireHeight && swapStates[_secretHashLock] == States.OPEN);
+    /// @param _randomNumberHash The hash of randomNumber and timestamp
+    function claimable(bytes32 _randomNumberHash) external view returns (bool) {
+        return (block.number < swaps[_randomNumberHash].expireHeight && swapStates[_randomNumberHash] == States.OPEN);
     }
 
-    /// @notice Calculate the secretHashLock from secretKey and timestamp
+    /// @notice Calculate the randomNumberHash from random number and timestamp
     ///
-    /// @param _secretKey The secret.
+    /// @param _randomNumber The random number.
     /// @param _timestamp The timestamp.
-    function calSecretHash(bytes32 _secretKey, uint64 _timestamp) public pure returns (bytes32) {
-        return sha256(abi.encodePacked(_secretKey, _timestamp));
+    function calRandomNumberHash(bytes32 _randomNumber, uint64 _timestamp) public pure returns (bytes32) {
+        return sha256(abi.encodePacked(_randomNumber, _timestamp));
     }
 }
